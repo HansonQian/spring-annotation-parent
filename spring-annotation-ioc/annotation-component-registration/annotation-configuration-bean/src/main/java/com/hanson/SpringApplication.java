27f013ed3504566705bf9b900ca78920c38f5ed5
HansonQian <hanson.qian@foxmail.com>
2019-09-17 10:35:13 +0800

    change name

--- spring-annotation-ioc/annotation-component-registration/annotation-configuration-bean/src/main/java/com/hanson/SpringApplication.java
+++ spring-annotation-ioc/annotation-component-registration/annotation-configuration-bean/src/main/java/com/hanson/SpringApplication.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(SpringConfiguration.class);
 
         Person person = applicationContext.getBean(Person.class);
 
